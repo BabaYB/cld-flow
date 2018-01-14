a187a3cff7d0d98b9086a94960fb1dfc6f78794b
BabaYB <836885645@qq.com>
2018-01-14 20:15:51 +0800

    src : add source code in simple.c

--- simple/simple.c
+++ simple/simple.c
@@ -1,1 +1,24 @@
-#include<stdio.h>
+//#include<stdio.h>
+/* simple.c */
+
+int printf(const char * format, ... );
+
+int global_init_var = 25;
+int global_uninit_var;
+
+void fun1 (int i)
+{
+  printf("%d\n", i);
+}
+
+int main (void)
+{
+  static int static_var = 11;
+  static int static_var2;
+  
+  int a = 1;
+  int b;
+  
+  func1(static_var + static_var2 + a + b);
+  return a;
+}
